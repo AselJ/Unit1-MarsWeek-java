8e13a55d365b9ef90601af7cbcda4cbd4323dc69
Asel <asya8427@gmail.com>
2023-08-17 00:58:40 -0600

    Finished part 1,2 and working on part 3

---
+++ src/GuessingGame.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.InputMismatchException;
+public class GuessingGame {
+    public static void main(String[] args){
+        //add code that displays a greeting to the player
+        System.out.println("Howdy, what's your name?");
+        System.out.println("(type in your name) ");
+
+        Scanner input = new Scanner(System.in);
+
+        String name = input.nextLine();
+
+    }
+}
---
+++ src/Mars.java
@@ -0,0 +1,53 @@
+public class Mars {
+    public static void main(String[] args) throws InterruptedException {
+        //The main method contains a String, int, double, and boolean
+        String colonyName = "Zazu"; //string
+        int shipPopulation = 300; //int
+        double meals = 4000.00; //double
+        boolean landing = true; //boolean
+
+        landingCheck(5);
+
+        meals = meals - (shipPopulation * 0.75);
+        meals = meals - (shipPopulation * 0.75);
+        System.out.println(meals);
+        meals = meals + (meals * .5);
+        shipPopulation = shipPopulation + 5;
+
+
+        //Include an if-else statement
+        String landingLocation = "The Ocean";
+        if (landingLocation.equalsIgnoreCase("The Plain")) {
+            System.out.println("Bbzzz landing on The Plain");
+        } else {
+            System.out.println("ERROR!!! Flight plan already set. Landing on The Plain");
+        }
+
+
+        //Call the landingCheck function
+
+
+        //Instantiate a GuessingGame object
+        //Instantiate a MarsExpedition object
+        //Instantiate a FindingsList object
+        //System.out.println(nameOfPlayer);
+    }
+
+    public static void landingCheck(int minutesLeft) throws InterruptedException {
+
+        for (int minute = 0; minute <= minutesLeft; minute++) {
+            if (((minute % 3) == 0) && ((minute % 2) == 0)) {
+                System.out.println("Keep Center");
+            } else if ((minute % 2) == 0) {
+                System.out.println("Right");
+            } else if ((minute % 3) == 0) {
+                System.out.println("Left");
+            } else {
+                System.out.println("Calculating");
+            }
+            Thread.sleep(250);
+        }
+        System.out.println("Landed");
+
+    }
+}
